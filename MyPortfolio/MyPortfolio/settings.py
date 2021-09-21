4fbbeb199eba66a60840236387aea954a6260fee
Caio Gomes <caicesardev@gmail.com>
2021-09-21 19:36:52 +0200

    Preparing deployment

--- MyPortfolio/MyPortfolio/settings.py
+++ MyPortfolio/MyPortfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l)hjr0=^=_p&r$j92$enu@+f9viw)cp079)o(=q%chz2i55k2m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['caiogomes.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
