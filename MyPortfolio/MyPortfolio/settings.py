0369c2d7e531d78a0971b8fe6dd42bf62109c153
Caio Gomes <caicesardev@gmail.com>
2021-09-21 20:55:34 +0200

    comma

--- MyPortfolio/MyPortfolio/settings.py
+++ MyPortfolio/MyPortfolio/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'portfolio.apps.PortfolioConfig'
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
