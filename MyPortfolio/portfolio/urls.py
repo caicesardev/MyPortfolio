2712549337a67f0a680cb8173d7d4c98dd2ccc59
Caio Gomes <caicesardev@gmail.com>
2021-09-21 19:59:22 +0200

    Dunno

--- MyPortfolio/portfolio/urls.py
+++ MyPortfolio/portfolio/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('portfolio/', views.portfolio, name="portfolio"),
-    path('contact/', views.contact, name="contact"),
-    path('blog/', views.blog, name="blog"),
+    path('', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('portfolio', views.portfolio, name="portfolio"),
+    path('contact', views.contact, name="contact"),
+    path('blog', views.blog, name="blog"),
 ]
