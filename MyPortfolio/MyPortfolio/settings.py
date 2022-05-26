4a7bece11c0c789f19b5b51da99f017c98bd4b2a
Caio Gomes <caicesardev@gmail.com>
2022-05-26 19:29:53 +0200

    Modified procfile

--- MyPortfolio/MyPortfolio/settings.py
+++ MyPortfolio/MyPortfolio/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'MyPortfolio.portfolio.apps.PortfolioConfig',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
--- MyPortfolio/MyPortfolio/wsgi.py
+++ MyPortfolio/MyPortfolio/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'MyPortfolio.MyPortfolio.settings')
+                      'MyPortfolio.settings')
 
 application = get_wsgi_application()
