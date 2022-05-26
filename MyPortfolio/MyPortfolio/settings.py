67c323d4647d13254dd8a536662906851ecf4218
Caio Gomes <caicesardev@gmail.com>
2022-05-26 19:28:38 +0200

    Modified procfile

--- MyPortfolio/MyPortfolio/settings.py
+++ MyPortfolio/MyPortfolio/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'portfolio.apps.PortfolioConfig',
+    'MyPortfolio.portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
