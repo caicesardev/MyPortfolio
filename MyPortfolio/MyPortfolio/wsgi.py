3b66ceb16ffde4d6fc9801eb733c2022f5263085
Caio Gomes <caicesardev@gmail.com>
2022-05-26 19:35:46 +0200

    Modified procfile

--- MyPortfolio/MyPortfolio/wsgi.py
+++ MyPortfolio/MyPortfolio/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'settings')
+                      'MyPortfolio.settings')
 
 application = get_wsgi_application()
